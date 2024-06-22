ea23a36669a3b14a85815044a9a86c4f69697859
preaus007 <touhedcs@gmail.com>
2024-06-23 00:13:44 +0600

    create pages and routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import CreatePost from './pages/CreatePost';
+import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
+import PostPage from './pages/PostPage';
+import Projects from './pages/Projects';
+import Search from './pages/Search';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import UpdatePost from './pages/UpdatePost';
 
 export default function App() {
   return (
-    <div className='text-3xl text-red-600'>
-      My Blog website.
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/search' element={<Search />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
